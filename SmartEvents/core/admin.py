29914b65b753e1dbf220ffccb9f4996f3f4903b8
MaxiD1 <maximiliano.duarte@usm.cl>
2025-10-21 19:09:49 -0300

    Se copio el codigo mostrado en clase para tener algo funcional (aún hay cambios que hacer)

--- SmartEvents/core/admin.py
+++ SmartEvents/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Proveedor, Producto, Contacto
 
-# Register your models here.
+
+admin.site.register(Producto)
+admin.site.register(Proveedor)
+admin.site.register(Contacto)
--- SmartEvents/core/views.py
+++ SmartEvents/core/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Contacto
 
-# Create your views here.
+def home(request):
+    titulo = "Inicio"
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    productos = ['computadores','periféricos','servidores']
+    
+    data = {
+        "titulo": titulo,
+        "productos":productos
+    }
+     
+    #return HttpResponse(titulo)
+    return render(request,'core/home.html',data)
+    
+def contacto(request):
+    titulo = "Contacto"
 
+    if request.POST:
+        #capturar el mensaje
+        nombre = request.POST["txtNombre"]
+        email = request.POST["txtEmail"]
+        mensaje = request.POST["txtMensaje"]
+
+        #validaciones de negocio
+
+        #crear nuevo objeto
+        nuevoContacto = Contacto()
+        nuevoContacto.nombre = nombre
+        nuevoContacto.email = email
+        nuevoContacto.mensaje = mensaje
+
+        nuevoContacto.save()
+
+
+
+    return render(request,'core/contacto.html')

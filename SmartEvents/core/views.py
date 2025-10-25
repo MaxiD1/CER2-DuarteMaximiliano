273d0d10ec98fa7a2ea19ef0ac10f1983de6dcc3
MaxiD1 <maximiliano.duarte@usm.cl>
2025-10-25 20:59:59 -0300

    Se creo un sisteme para inicial sesion

--- SmartEvents/core/views.py
+++ SmartEvents/core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import evento
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def home(request):
     #return HttpResponse(titulo)
@@ -13,3 +14,15 @@ def events(request):
 def community(request):
     #return HttpResponse(titulo)
     return render(request,'core/community.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'core/register.html', {'form': form})
